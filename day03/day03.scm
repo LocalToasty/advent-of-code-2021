7117301d07bcc7cf2dab5551499b0d995848e16e
Marko van Treeck <markovantreeck@gmail.com>
2021-12-03 16:06:12 +0100

    Make day 3 part 2 more efficient

--- day03/day03.scm
+++ day03/day03.scm
@@ -5,6 +5,9 @@
         (scheme vector)
         (scheme bitwise))
 
+;;> Reads input to list.
+
+;;> Each line of the input is transformed into a string of bools
 (define (read-input filename)
   (with-input-from-file filename
     (lambda ()
@@ -19,21 +22,28 @@
   (let-values (((gamma epsilon) (gamma-epsilon bits)))
     (* gamma epsilon)))
 
-(define (gamma-epsilon bits)
-  (let* ((majority (ceiling (/ (length bits) 2)))
-	 (counts (bitcounts bits))
+(define (gamma-epsilon rows)
+  (let* ((majority (ceiling (/ (length rows) 2)))
+         (counts (bitcounts rows))
+         ; calculate a number where each bit is the one occurring most often in
+         ; that column.
          (gamma (bitvec->int (vector-map
                                (lambda (count) (>= count majority))
                                counts)))
-         (epsilon (- (expt 2 (vector-length (car bits)))
+         ; one's complement of gamma
+         (epsilon (- (expt 2 (vector-length (car rows)))
                      gamma 1)))
+
   (values gamma epsilon)))
 
-(define (bitcounts bits)
-  (let ((bits (map (lambda (bits) (vector-map (lambda (b) (if b 1 0)) bits))
-                   bits)))
-    (apply vector-map + bits)))
+;;> Calculates how many of the bits are 1 in each column
+(define (bitcounts rows)
+  (let ((rows (map (lambda (rows)
+                     (vector-map (lambda (b) (if b 1 0)) rows))
+                   rows)))
+    (apply vector-map + rows)))
 
+;;> Transforms a vector of bools into an integer
 (define (bitvec->int v)
   (vector-fold (lambda (x b) (bitwise-ior (arithmetic-shift x 1)
                                           (if b 1 0)))
@@ -45,21 +55,25 @@
 (define (o2-generator-rating bits) (rating bits >=))
 (define (co2-scrubber-rating bits) (rating bits <))
 
-(define (rating bits cmp)
+(define (rating rows cmp)
   (bitvec->int
-    (let loop ((bits bits)
+    (let loop ((rows rows)
+               ; bit to look at
                (i 0))
-      (cond ((null-list? (cdr bits)) (car bits))
-            ((>= i (vector-length (car bits))) (car bits))
-            (else
-        (let* ((count (vector-ref (bitcounts bits) i))
-               (most-common (cmp count
-                                 (- (length bits) count))))
-          (loop
-            (filter (lambda (b) (eqv? (vector-ref b i)
-                                      most-common))
-                    bits)
-            (+ i 1))))))))
+      (cond
+        ((null-list? (cdr rows)) (car rows))        ; only one entry left
+        ((>= i (vector-length (car rows))) (car rows))        ; all bits done
+        (else
+          (let* (; number of one-bits in the ith column
+                 (count (fold (lambda (row sum) (if (vector-ref row i) (+ sum 1) sum))
+                              0
+                              rows))
+                 ; find bit value to be kept
+                 (to-be-kept (cmp count (- (length rows) count))))
+            (loop
+              (filter (lambda (b) (eqv? to-be-kept (vector-ref b i)))
+                      rows)
+              (+ i 1))))))))
 
 (define bits (read-input (cadr (command-line))))
 (write (part1 bits)) (newline)
